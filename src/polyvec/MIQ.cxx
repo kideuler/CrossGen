fac754df61ac630d5405275b8ce613db1ee5dc86
Jacob Jones <darkstorm143@gmail.com>
2026-01-14 10:58:36 -0700

    added CoMiSo

---
+++ src/polyvec/MIQ.cxx
@@ -0,0 +1,1 @@
+#include "MIQ.hxx"
---
+++ src/polyvec/MIQ.hxx
@@ -0,0 +1,8 @@
+#ifndef __MIQ_HXX__
+#define __MIQ_HXX__
+
+#include <CoMISo/Solver/ConstrainedSolver.hh>
+#include <CoMISo/Solver/MISolver.hh>
+#include <CoMISo/Solver/GMM_Tools.hh>
+
+#endif
